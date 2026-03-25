f35076c9e307d9f5c5c5dc57d023cf8d88472945
skydogzz <tstephan00@outlook.com>
2026-03-25 19:47:42 +0100

    oui

--- cpp07/ex01/Iter.hpp
+++ cpp07/ex01/Iter.hpp
@@ -3,26 +3,26 @@
 
 #include <cstddef>
 
-template <typename T>
-void iter(T array[], const unsigned int &size, void (&f)(T &x)) {
-  for (unsigned int i = 0; i < size; i++)
-    f(array[i]);
+template <typename T> void iter(T array[], const unsigned int& size, void (&f)(T& x))
+{
+    for (unsigned int i = 0; i < size; i++)
+        f(array[i]);
 }
 
-template <typename T>
-void iter(T *array, std::size_t const length, void (*f)(T &)) {
-  if (!array || !f)
-    return;
-  for (std::size_t i = 0; i < length; ++i)
-    f(array[i]);
+template <typename T> void iter(T* array, std::size_t const length, void (*f)(T&))
+{
+    if (!array || !f)
+        return;
+    for (std::size_t i = 0; i < length; ++i)
+        f(array[i]);
 }
 
-template <typename T>
-void iter(T const *array, std::size_t const length, void (*f)(T const &)) {
-  if (!array || !f)
-    return;
-  for (std::size_t i = 0; i < length; ++i)
-    f(array[i]);
+template <typename T> void iter(T const* array, std::size_t const length, void (*f)(T const&))
+{
+    if (!array || !f)
+        return;
+    for (std::size_t i = 0; i < length; ++i)
+        f(array[i]);
 }
 
 #endif
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,62 +1,76 @@
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
+#ifndef ARRAYm_HPP
+#define ARRAYm_HPP
 
 #include <cstddef>
 #include <stdexcept>
 
 template <typename T> class Array {
 private:
-  T *_elem;
-  size_t _size;
+    T* m_elem;
+    unsigned int m_size;
 
 public:
-  Array() : _elem(NULL), _size(0) {}
-
-  Array(size_t n) : _elem(new T[n]), _size(n) {}
-
-  Array(const Array &other) : _elem(NULL), _size(0) {
-    if (other._size > 0) {
-      _elem = new T[other._size];
-      _size = other._size;
-      for (size_t i = 0; i < _size; ++i) {
-        _elem[i] = other._elem[i];
-      }
+    Array()
+        : m_elem(NULL)
+        , m_size(0)
+    {
     }
-  }
 
-  ~Array() { delete[] _elem; }
+    Array(unsigned int n)
+        : m_elem(new T[n])
+        , m_size(n)
+    {
+    }
 
-  Array &operator=(const Array &other) {
-    if (this != &other) {
-      delete[] _elem;
-
-      _elem = NULL;
-      _size = 0;
-
-      if (other._size > 0) {
-        _elem = new T[other._size];
-        _size = other._size;
-        for (size_t i = 0; i < _size; ++i) {
-          _elem[i] = other._elem[i];
+    Array(const Array& other)
+        : m_elem(NULL)
+        , m_size(0)
+    {
+        if (other.m_size > 0) {
+            m_elem = new T[other.m_size];
+            m_size = other.m_size;
+            for (unsigned int i = 0; i < m_size; ++i) {
+                m_elem[i] = other.m_elem[i];
+            }
         }
-      }
     }
-    return *this;
-  }
 
-  T &operator[](size_t i) {
-    if (i >= _size)
-      throw std::out_of_range("Array index out of range");
-    return _elem[i];
-  }
+    ~Array() { delete[] m_elem; }
 
-  const T &operator[](size_t i) const {
-    if (i >= _size)
-      throw std::out_of_range("Array index out of range");
-    return _elem[i];
-  }
+    Array& operator=(const Array& other)
+    {
+        if (this != &other) {
+            delete[] m_elem;
 
-  size_t size() const { return _size; }
+            m_elem = NULL;
+            m_size = 0;
+
+            if (other.m_size > 0) {
+                m_elem = new T[other.m_size];
+                m_size = other.m_size;
+                for (unsigned int i = 0; i < m_size; ++i) {
+                    m_elem[i] = other.m_elem[i];
+                }
+            }
+        }
+        return *this;
+    }
+
+    T& operator[](unsigned int i)
+    {
+        if (i >= m_size)
+            throw std::out_of_range("Array index out of range");
+        return m_elem[i];
+    }
+
+    const T& operator[](unsigned int i) const
+    {
+        if (i >= m_size)
+            throw std::out_of_range("Array index out of range");
+        return m_elem[i];
+    }
+
+    unsigned int size() const { return m_size; }
 };
 
 #endif
